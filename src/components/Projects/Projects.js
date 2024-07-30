db3d03bcedfa2206a213146b6bf12039799e3dac
Harman1107 <2020kuec2001@iiitkota.ac.in>
2024-07-30 20:17:05 +0530

    changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -34,11 +34,33 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Online Judge"
+              description="Engineered an Online Judge platform enabling real-time code execution and evaluation for correctness, identifying compilation errors.mplemented a ReactJS frontend integrated with a NodeJS backend, Mongodb database, and JWT token authentication. Explored Docker techniques for code isolation to enhance security against untrusted codes."
+              ghLink="https://github.com/DevAgrawal1112/OJ"
+              demoLink="ec2-13-127-199-105.ap-south-1.compute.amazonaws.com:3000"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Ecommerce Warranty System Using NFTs"
-              description="Spearheaded the creation of an NFT-based digital warranty card, detailing product information and tracking ownership. Developed features including a Sign Up-Log In System, CRUD operations on products, engaging user games, product transfers, and ownership verification.Deployed the smart contract using ERC-721 NFT tokens for seamless token transfers between peers."
-              ghLink="https://github.com/DevAgrawal1112/Blockchain-based-eCommerce-Warranty-system-fg4"
+              title="Poker-Game"
+              description="Utilized www.deckofcardsapi.com to create a seamless and engaging card game experience. Implemented Socket.io
+              to develop real-time communication between players during the game. Integrated responsive design principles into
+              the development of the game interface. (ReactJS, NodeJS, MongoDB, Socket.io)"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Virtual Classroom using Unreal Engine"
+              description="Built a virtual environment for students deprived of access to specialized Labs. Implemented 3 labs (2 Chemistry
+              Labs, 1 Astronomy Lab). Tech Stack Used:- Unity (Virtual Environment using C), ReactJS( UI ), NodeJS ( Backend
+              ), MetaMask ( Secure Login ), MongoDB."
             />
           </Col>
 
@@ -48,7 +70,7 @@ function Projects() {
               isBlog={false}
               title="Suno Gaana"
               description="Engineered a Music Player displaying top songs, artists, and native songs based on the user’s location. Used Shazam Core API and Geo Location API for loading songs and live location of a user respectively. Developed the frontend using ReactJS, tailwind, and incorporated Redux"
-              ghLink="https://github.com/DevAgrawal1112/suno-gaana"         
+              ghLink="https://github.com/DevAgrawal1112/suno-gaana"
             />
           </Col>
         </Row>
